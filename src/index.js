be8c1296463ae9fb6dcd11d2437dd1e1a4405d36
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-02-12 21:19:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   let morsearr = [];
+	let tenzero = [];
+	let arrfinal = [];
+
+	for (let i = 0; i < expr.length; i++) {
+		for (let key in MORSE_TABLE) {
+			if (expr[i] === MORSE_TABLE[key]) {
+				morsearr.push(key)
+			}
+		}
+	}
+	for (let i = 0; i < morsearr.length; i++) {
+		let arrayI = "";
+		for (let k = 0; k < morsearr[i].length; k++) {
+			if (morsearr[i][k] === "-") {
+				arrayI = arrayI + "11"
+			} else if (morsearr[i][k] === ".") {
+				arrayI = arrayI + "10"
+			}
+		}
+		tenzero.push(arrayI)
+	}
+
+	for (let i = 0; i < tenzero.length; i++) {
+		let zerocount = "";
+		let strlength = 0;
+		strlength = 10 - tenzero[i].length;
+		for (let j = 0; j < strlength; j++) {
+			zerocount = zerocount + "0"
+		}
+		arrfinal[i] = zerocount + tenzero[i];
+	}
+	return (arrfinal.join(""))
 }
 
 module.exports = {
