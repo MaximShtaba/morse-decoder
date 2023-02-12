96bb941aa87caaa3310dd20d619c033867da3194
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-02-12 23:44:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,78 +1,101 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+	'.-': 'a',
+	'-...': 'b',
+	'-.-.': 'c',
+	'-..': 'd',
+	'.': 'e',
+	'..-.': 'f',
+	'--.': 'g',
+	'....': 'h',
+	'..': 'i',
+	'.---': 'j',
+	'-.-': 'k',
+	'.-..': 'l',
+	'--': 'm',
+	'-.': 'n',
+	'---': 'o',
+	'.--.': 'p',
+	'--.-': 'q',
+	'.-.': 'r',
+	'...': 's',
+	'-': 't',
+	'..-': 'u',
+	'...-': 'v',
+	'.--': 'w',
+	'-..-': 'x',
+	'-.--': 'y',
+	'--..': 'z',
+	'.----': '1',
+	'..---': '2',
+	'...--': '3',
+	'....-': '4',
+	'.....': '5',
+	'-....': '6',
+	'--...': '7',
+	'---..': '8',
+	'----.': '9',
+	'-----': '0',
 };
 
+
+
 function decode(expr) {
-   let morsearr = [];
+	let morseArr = [];
+	let morsePoint = [];
 	let tenzero = [];
-	let arrfinal = [];
-
+	let arrayOutput = [];
 	for (let i = 0; i < expr.length; i++) {
-		for (let key in MORSE_TABLE) {
-			if (expr[i] === MORSE_TABLE[key]) {
-				morsearr.push(key)
+		morseArr.push(expr[i])
+	}
+	for (let i = 0; i < morseArr.length; i++) {
+		
+		if (morseArr[i] !== " ") {
+			for (let key in MORSE_TABLE) {
+				if (morseArr[i] === MORSE_TABLE[key]) {
+					morsePoint.push(key)
+				}
 			}
 		}
-	}
-	for (let i = 0; i < morsearr.length; i++) {
-		let arrayI = "";
-		for (let k = 0; k < morsearr[i].length; k++) {
-			if (morsearr[i][k] === "-") {
-				arrayI = arrayI + "11"
-			} else if (morsearr[i][k] === ".") {
-				arrayI = arrayI + "10"
-			}
+		else if (morseArr[i] === " ") {
+			morsePoint.push("**********")
 		}
-		tenzero.push(arrayI)
 	}
-
+	for (let i = 0; i < morsePoint.length; i++) {
+		
+		if (morsePoint[i] !== "**********") {
+			let arrayI = "";
+			for (let j = 0; j < morsePoint[i].length; j++) {
+				if (morsePoint[i][j] === "-") {
+					arrayI = arrayI + "11"
+				} else if (morsePoint[i][j] === ".") {
+					arrayI = arrayI + "10"
+				}
+			}
+			tenzero.push(arrayI)
+		}
+		else if (morsePoint[i] === "**********") {
+			tenzero.push("**********")
+		}
+	}
 	for (let i = 0; i < tenzero.length; i++) {
 		let zerocount = "";
 		let strlength = 0;
 		strlength = 10 - tenzero[i].length;
-		for (let j = 0; j < strlength; j++) {
-			zerocount = zerocount + "0"
+		if (tenzero[i] !== "**********") {
+			for (let j = 0; j < strlength; j++) {
+				zerocount = zerocount + "0"
+			}
+			arrayOutput[i] = zerocount + tenzero[i];
 		}
-		arrfinal[i] = zerocount + tenzero[i];
+		else if (tenzero[i] === "**********") {
+			arrayOutput.push("**********")
+		}
 	}
-	return (arrfinal.join(""))
+	return(arrayOutput.join(""))
 }
 
+
+decode(expr)
 module.exports = {
-    decode
+	decode
 }
